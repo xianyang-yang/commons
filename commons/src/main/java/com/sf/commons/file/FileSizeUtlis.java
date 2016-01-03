603e9e71e3c0af57c58647693fa760a5139f6c81
xianyang.yang <xianyang_yang@126.com>
2016-01-03 22:06:16 +0800

    优化filesizeUtils

--- commons/src/main/java/com/sf/commons/file/FileSizeUtlis.java
+++ commons/src/main/java/com/sf/commons/file/FileSizeUtlis.java
@@ -1,5 +1,7 @@
 package com.sf.commons.file;
 
+import static org.apache.commons.lang.StringUtils.stripEnd;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
@@ -10,7 +12,7 @@ import java.math.RoundingMode;
  * HISTORY
  * ****************************************************************************
  *  ID   DATE           	   		PERSON          REASON
- *  1    2015年10月12日 上午11:00:10		204401          Create
+ *  1    2015年10月12日 上午11:00:10	204401          Create
  * ****************************************************************************
  * </pre>
  * 
@@ -20,29 +22,28 @@ import java.math.RoundingMode;
 public class FileSizeUtlis {
 	private static final int _1K = 1024;
 
-	private static final char[] levelName = new char[] { 'B', 'K', 'M', 'G','T' };
-	
+	/**
+	 * 支持的单位
+	 */
+	private static final char[] sizeUnits = new char[] { 'B', 'K', 'M', 'G', 'T', 'P' };
+
 	/**
 	 * 将文件大小从字节数转为可读的字符串
-	 * @param byteSize 字节数
+	 * 
+	 * @param byteSize
+	 *            字节数
 	 * @return
 	 */
 	public static String humanSize(long byteSize) {
 		int level = 0;
 		double result = (double) byteSize;
-		while (result / _1K >= 1) {
+		while (result >= _1K && level < sizeUnits.length - 1) {
 			result /= _1K;
 			level++;
 		}
-		return new BigDecimal(result).setScale(2, RoundingMode.HALF_UP)
-				.toString() + getMemUnit(level);
+		String size = new BigDecimal(result).setScale(2, RoundingMode.HALF_UP).toString();
+		size=stripEnd(stripEnd(size, "0"),".");
+		return size+ sizeUnits[level];
 
-	}
-
-	private static char getMemUnit(int level) {
-		if (level >= levelName.length) {
-			level = levelName.length - 1;
-		}
-		return levelName[level];
 	}
 }
